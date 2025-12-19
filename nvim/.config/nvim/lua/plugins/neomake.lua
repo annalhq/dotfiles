7f742a9d8384d87966c33ed46b8ba19556262d7d
annalhq <annalhqshaikh@gmail.com>
2025-12-19 20:16:28 +0530

    chore: cleanup

--- nvim/.config/nvim/lua/plugins/neomake.lua
+++ nvim/.config/nvim/lua/plugins/neomake.lua
@@ -1,11 +1,9 @@
 return {
-  -- Add Neomake plugin to the list of plugins
   {
     "neomake/neomake",
     config = function()
-      -- Optional: Configure Neomake settings if needed
-      vim.g.neomake_open_list = 2 -- Automatically open the make result list
-      vim.g.neomake_cpp_enabled_makers = { "g++" } -- Enable g++ for C++ compilation
+      vim.g.neomake_open_list = 2
+      vim.g.neomake_cpp_enabled_makers = { "g++" }
     end,
   },
 }
