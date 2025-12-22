84454b8ddfa60c453369ac77a5e0be4fd704bac1
annalhq <annalhqshaikh@gmail.com>
2025-12-23 01:14:04 +0530

    feat(nvim): integrate nvim supertab with luasnip
    - remove conflicting tab/<s-tab> luasnip mapping from init.lua
    - adpot nvim-cmp supertab mapping integration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,14 +2,6 @@ require("config.lazy")
 
 require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/LuaSnip/" })
 
-vim.cmd([[
-imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
-smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
-
-imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
-smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
-]])
-
 vim.api.nvim_create_user_command("CmpBorderDebug", function()
   local ok_cmp = pcall(require, "cmp")
   local ok_blink = pcall(require, "blink.cmp")
--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -3,31 +3,51 @@ return {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
 
       -- auto brackets
       opts.auto_brackets = { "python", "cpp", "c" }
 
-      -- keymaps
-      opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        else
-          fallback()
-        end
-      end, { "i", "s" })
-
-      opts.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        else
-          fallback()
-        end
-      end, { "i", "s" })
-
+      -- windows
       opts.window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
       }
+
+      -- supertab mapping
+      opts.mapping = vim.tbl_extend("force", opts.mapping or {}, {
+        ["<CR>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            if luasnip.expandable() then
+              luasnip.expand()
+            else
+              cmp.confirm({ select = true })
+            end
+          else
+            fallback()
+          end
+        end),
+
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.locally_jumpable(1) then
+            luasnip.jump(1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
 
       return opts
     end,
