eddf7b71428fa518a111692022a5bd719dc0365a
annalhq <annalhqshaikh@gmail.com>
2025-12-23 01:16:29 +0530

    chore(nvim): remove cmp debugging

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,9 +1,3 @@
 require("config.lazy")
 
 require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/LuaSnip/" })
-
-vim.api.nvim_create_user_command("CmpBorderDebug", function()
-  local ok_cmp = pcall(require, "cmp")
-  local ok_blink = pcall(require, "blink.cmp")
-  print("nvim-cmp loaded: " .. tostring(ok_cmp) .. " | blink.cmp loaded: " .. tostring(ok_blink))
-end, {})
