6bfabc2e0040c39dd9212cefb843a7588c3059ef
annalhq <annalhqshaikh@gmail.com>
2025-12-24 00:33:08 +0530

    chore: remove redundant colorschemes

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -26,7 +26,6 @@ require("lazy").setup({
     lazy = false,
     version = false,
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
     enabled = true,
     notify = false,
